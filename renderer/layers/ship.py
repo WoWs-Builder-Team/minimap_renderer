55dc12888e17640587a78b5ba11f8a42e02ea86d
notyourfather <applicatnccnt@gmail.com>
2022-07-31 23:57:21 +0800

    Added consumable icons. Sorted the icons.

--- renderer/layers/ship.py
+++ renderer/layers/ship.py
@@ -26,6 +26,10 @@ class LayerShip(LayerBase):
         self._ship_info = generate_ship_data(
             self._renderer.replay_data.player_info
         )
+        self._active_consumables: dict[int, dict[int, float]] = {}
+        self._abilities = renderer.resman.load_json(
+            "renderer.resources", "abilities.json"
+        )
 
     def draw(self, game_time: int, image: Image.Image):
         """Yields an arguments for Image.paste.
@@ -40,11 +44,29 @@ class LayerShip(LayerBase):
         player_info = self._renderer.replay_data.player_info
         events = self._renderer.replay_data.events
         scaling = self._renderer.scaling
+        players_consumables = events[game_time].evt_consumable
 
-        for vehicle_id, vehicle in events[game_time].evt_vehicle.items():
+        if players_consumables := events[game_time].evt_consumable:
+            for player_consumables in players_consumables.values():
+                for player_consumable in player_consumables:
+                    acs = self._active_consumables.setdefault(
+                        player_consumable.ship_id, {}
+                    )
+                    acs[player_consumable.consumable_id] = round(
+                        player_consumable.duration
+                    )
+
+        for vehicle in sorted(
+            events[game_time].evt_vehicle.values(),
+            key=lambda s: (s.is_alive, s.is_visible),
+        ):
             name, species, level, holder = self._ship_info[vehicle.avatar_id]
 
+            # if consumables_used := consumables.get(vehicle_id, None):
+            #     print(consumables_used)
+
             player = player_info[vehicle.avatar_id]
+
             icon = self._ship_icon(
                 vehicle.is_alive,
                 vehicle.is_visible,
@@ -61,20 +83,20 @@ class LayerShip(LayerBase):
                 else COLORS_NORMAL[vehicle.relation]
             )
 
-            # print(
-            #     vehicle.relation, vehicle.is_visible, vehicle.visibility_flag
-            # )
-
             if vehicle.is_alive:
                 if vehicle.is_visible:
                     holder = holder.copy()
+
+                    self._ship_consumable(
+                        holder, vehicle.vehicle_id, player.ship_params_id
+                    )
 
                     if vehicle.visibility_flag > 0 and vehicle.relation in [
                         -1,
                         0,
                     ]:
                         draw = ImageDraw.Draw(holder)
-                        draw.ellipse([(20, 20), (25, 25)], fill="orange")
+                        draw.ellipse([(27, 38), (30, 42)], fill="orange")
 
                     if not vehicle.not_in_range:
                         draw_health_bar(
@@ -120,6 +142,42 @@ class LayerShip(LayerBase):
             else:
                 image.paste(**paste_args_centered(icon, x, y, True))
 
+        # Decrement consumable timer and pop if 0
+
+        for apcs in list(self._active_consumables.keys()):
+            for apc in list(self._active_consumables[apcs]):
+                if self._active_consumables[apcs][apc] > 0:
+                    self._active_consumables[apcs][apc] -= 1
+                else:
+                    self._active_consumables[apcs].pop(apc)
+
+                    if not self._active_consumables[apcs]:
+                        self._active_consumables.pop(apcs)
+
+    def _ship_consumable(
+        self, image: Image.Image, vehicle_id: int, params_id: int
+    ):
+        if ac := self._active_consumables.get(vehicle_id, {}):
+            c_icons_holder = Image.new("RGBA", (20 * len(ac), 20))
+            x_pos = 0
+
+            for aid, duration in ac.items():
+                cname = self._abilities[str(params_id)][str(aid)]
+                filename = f"consumable_{cname}.png"
+                c_image = self._renderer.resman.load_image(
+                    "renderer.resources.consumables", filename, size=(20, 20)
+                )
+                c_icons_holder.paste(c_image, (x_pos, 0), c_image)
+                x_pos += 20
+
+            image.paste(
+                c_icons_holder,
+                (int(image.width / 2 - c_icons_holder.width / 2), 0),
+                c_icons_holder,
+            )
+            # image.paste(c_image, (x_pos, 10), c_image)
+            # x_pos -= 10
+
     def _ship_icon(
         self,
         is_alive: bool,
@@ -159,6 +217,9 @@ class LayerShip(LayerBase):
             else:
                 icon_type = "dead"
 
-        return load_image(
+        # return load_image(
+        #     f"renderer.resources.ship_icons.{icon_type}", f"{species}.png"
+        # )
+        return self._renderer.resman.load_image(
             f"renderer.resources.ship_icons.{icon_type}", f"{species}.png"
         )
