6b290575c7631bc750ff4472e81057b888eca3be
notyourfather <applicatnccnt@gmail.com>
2022-08-01 20:25:25 +0800

    Added versioned LayerShot for testing.

---
+++ renderer/versions/0_11_6/layers/__init__.py
@@ -0,0 +1,1 @@
+from .shot import LayerShot
---
+++ renderer/versions/0_11_6/layers/shot.py
@@ -0,0 +1,12 @@
+from renderer.layers import LayerShotBase
+from renderer.render import Renderer
+from PIL import ImageDraw
+
+
+class LayerShot(LayerShotBase):
+    pass
+    # def __init__(self, renderer: Renderer):
+    #     super().__init__(renderer)
+
+    # def draw(self, game_time: int, draw: ImageDraw.ImageDraw):
+    #     return super().draw(game_time, draw)
