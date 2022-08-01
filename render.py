e8313fa8f3827a8e8d2ecc18332132c977d79852
notyourfather <applicatnccnt@gmail.com>
2022-08-01 20:23:58 +0800

    Removed comment.

--- render.py
+++ render.py
@@ -8,6 +8,5 @@ if __name__ == "__main__":
     with open("data.dat", "rb") as f:
         a = time.perf_counter()
         Renderer(pickle.load(f)).start()
-        # get_renderer("0_11_6")(pickle.load(f)).start()
         b = time.perf_counter()
         print(b - a)
