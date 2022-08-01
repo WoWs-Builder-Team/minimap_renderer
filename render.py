436da99756665838a0217b0a1e9e487ee93b6e9c
notyourfather <applicatnccnt@gmail.com>
2022-08-01 15:26:10 +0800

    Removed loading of versioned renderer.

--- render.py
+++ render.py
@@ -1,11 +1,13 @@
 import pickle
 
-from renderer import get_renderer
+from renderer.render import Renderer
+
 import time
 
 if __name__ == "__main__":
     with open("data.dat", "rb") as f:
         a = time.perf_counter()
-        get_renderer("0_11_6")(pickle.load(f)).start()
+        Renderer(pickle.load(f)).start()
+        # get_renderer("0_11_6")(pickle.load(f)).start()
         b = time.perf_counter()
         print(b - a)
