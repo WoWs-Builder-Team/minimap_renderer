b7566dc9fa4da95e3f51330fc5192449fff0a2d4
notyourfather <applicatnccnt@gmail.com>
2022-08-03 01:33:08 +0800

    Added new layers. Added method to scale points properly.

--- renderer/render.py
+++ renderer/render.py
@@ -1,6 +1,6 @@
 import json
 
-from typing import Optional
+from typing import Optional, Union
 from importlib.resources import open_text
 from importlib import import_module
 
@@ -10,6 +10,8 @@ from renderer.resman import ResourceManager
 from renderer.exceptions import MapLoadError, MapManifestLoadError
 from PIL import Image, ImageDraw
 from imageio_ffmpeg import write_frames
+
+Number = Union[int, float]
 
 
 class Renderer:
@@ -23,6 +25,7 @@ class Renderer:
         self.res: str = f"{__package__}.resources"
         # MAP INFO
         self.minimap_image: Optional[Image.Image] = None
+        self.minimap_bg: Optional[Image.Image] = None
         self.minimap_size: int = 0
         self.space_size: int = 0
         self.scaling: float = 0.0
@@ -33,12 +36,16 @@ class Renderer:
         self._load_map()
 
         assert self.minimap_image
+        assert self.minimap_bg
 
         (
             layer_ship,
             layer_shot,
             layer_torpedo,
             layer_smoke,
+            layer_plane,
+            layer_ward,
+            layer_capture,
         ) = self._check_versioned_layers()
 
         video_writer = write_frames(
@@ -46,19 +53,26 @@ class Renderer:
             fps=20,
             quality=9,
             pix_fmt_in="rgba",
-            macro_block_size=19,
-            size=self.minimap_image.size,
+            macro_block_size=20,
+            size=self.minimap_bg.size,
         )
         video_writer.send(None)
 
         for game_time in self.replay_data.events.keys():
             minimap_img = self.minimap_image.copy()
+            minimap_bg = self.minimap_bg.copy()
+
             draw = ImageDraw.Draw(minimap_img)
+            layer_capture.draw(game_time, minimap_img)
+            layer_ward.draw(game_time, minimap_img)
             layer_shot.draw(game_time, draw)
             layer_torpedo.draw(game_time, draw)
             layer_ship.draw(game_time, minimap_img)
             layer_smoke.draw(game_time, minimap_img)
-            video_writer.send(minimap_img.tobytes())
+            layer_plane.draw(game_time, minimap_img)
+
+            minimap_bg.paste(minimap_img, (40, 40))
+            video_writer.send(minimap_bg.tobytes())
         video_writer.close()
 
     def _load_map(self):
@@ -74,8 +88,15 @@ class Renderer:
         map_res = self._load_map_manifest()
 
         try:
+            map_legends = self.resman.load_image(
+                self.res, "minimap_grid_legends.png"
+            )
             map_land = self.resman.load_image(map_res, "minimap.png")
             map_water = self.resman.load_image(map_res, "minimap_water.png")
+
+            self.minimap_bg = map_water.copy().resize(map_legends.size)
+            self.minimap_bg.paste(map_legends, mask=map_legends)
+
             map_water = Image.alpha_composite(map_water, draw_grid())
             self.minimap_image = Image.alpha_composite(map_water, map_land)
         except (FileNotFoundError, ModuleNotFoundError) as e:
@@ -130,12 +151,49 @@ class Renderer:
         else:
             return map_res
 
+    def get_scaled(
+        self, xy: tuple[Number, Number], flip_y=True
+    ) -> tuple[int, int]:
+        """Scales a coordinate properly.
+
+        Args:
+            xy (tuple[Number, Number]): Coordinate.
+            flip_y (bool, optional): Flips the y component. Defaults to True.
+
+        Returns:
+            tuple[int, int]: Scaled coordinated.
+        """
+        x, y = xy
+
+        if flip_y:
+            y = -y
+
+        x = round(x * self.scaling + self.minimap_size / 2)
+        y = round(y * self.scaling + self.minimap_size / 2)
+        return x, y
+
+    def get_scaled_r(self, r: Number):
+        return r * self.scaling
+
     def _check_versioned_layers(self):
+        """Check for versioned layer(s) and loads it.
+
+        Returns:
+            _type_: Initialized layers.
+        """
         versioned_layers_pkg = (
             f"{__package__}.versions.{self.replay_data.game_version}"
         )
 
-        layers = ["LayerShip", "LayerShot", "LayerTorpedo", "LayerSmoke"]
+        layers = [
+            "LayerShip",
+            "LayerShot",
+            "LayerTorpedo",
+            "LayerSmoke",
+            "LayerPlane",
+            "LayerWard",
+            "LayerCapture",
+        ]
         init_layers = []
 
         LOGGER.info("Looking for versioned layers")
