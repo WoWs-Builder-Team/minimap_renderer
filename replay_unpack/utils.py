1553dfaf451a5e4d24dfe22d050c311c2e156032
notyourfather <applicatnccnt@gmail.com>
2022-08-03 01:38:04 +0800

    Added functions to unpack plane ids.

--- replay_unpack/utils.py
+++ replay_unpack/utils.py
@@ -1,12 +1,14 @@
 def unpack_value(packed_value, value_min, value_max, bits):
-    return packed_value / (2 ** bits - 1) * (abs(value_min) + abs(value_max)) - abs(value_min)
+    return packed_value / (2**bits - 1) * (
+        abs(value_min) + abs(value_max)
+    ) - abs(value_min)
 
 
 def unpack_values(packed_value, pack_pattern):
     values = []
     for i, pattern in enumerate(pack_pattern):
         min_value, max_value, bits = pattern
-        value = packed_value & (2 ** bits - 1)
+        value = packed_value & (2**bits - 1)
 
         values.append(unpack_value(value, min_value, max_value, bits))
         packed_value = packed_value >> bits
@@ -14,4 +16,15 @@ def unpack_values(packed_value, pack_pattern):
         assert packed_value == 0
     except AssertionError:
         pass
+    return tuple(values)
+
+
+def unpack_plane_id(packed_value: int) -> tuple:
+    # avatar_id, index, purpose, departures
+    bits = [32, 3, 3, 1]
+    values = []
+    for bit in bits:
+        value = packed_value & (2**bit - 1)
+        packed_value = packed_value >> bit
+        values.append(value)
     return tuple(values)
