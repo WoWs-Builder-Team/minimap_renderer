27b41b2dbb60a9997daa4275e2dbc4a1de7f2187
notyourfather <applicatnccnt@gmail.com>
2022-08-03 01:32:13 +0800

    Added comments to the exceptions.

--- renderer/exceptions.py
+++ renderer/exceptions.py
@@ -2,8 +2,18 @@ from .utils import LOGGER
 
 
 class MapLoadError(Exception):
+    """Raised when there's an error at loading the map file(s).
+
+    Args:
+        Exception (_type_): _description_
+    """
     pass
 
 
 class MapManifestLoadError(Exception):
+    """Raised when there's an error at parsing the manifest file.
+
+    Args:
+        Exception (_type_): _description_
+    """
     pass
