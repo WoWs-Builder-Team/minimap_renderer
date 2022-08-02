5d51fa2b52837fd19b318041488029e096ae053f
notyourfather <applicatnccnt@gmail.com>
2022-08-03 01:35:39 +0800

    Added new layers.

--- renderer/layers/__init__.py
+++ renderer/layers/__init__.py
@@ -2,3 +2,6 @@ from .ship import LayerShipBase
 from .smoke import LayerSmokeBase
 from .shot import LayerShotBase
 from .torpedo import LayerTorpedoBase
+from .plane import LayerPlaneBase
+from .ward import LayerWardBase
+from .capture import LayerCaptureBase
