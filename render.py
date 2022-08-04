4c1492a3253fa31f4cc47047e9425e8ed60cb3d0
notyourfather <applicatnccnt@gmail.com>
2022-08-04 11:20:26 +0800

    Removed perfcounter.

--- render.py
+++ render.py
@@ -2,11 +2,6 @@ import pickle
 
 from renderer.render import Renderer
 
-import time
-
 if __name__ == "__main__":
     with open("data.dat", "rb") as f:
-        a = time.perf_counter()
         Renderer(pickle.load(f)).start()
-        b = time.perf_counter()
-        print(b - a)
