46dc45a571dda5c8fa9916ea6003be69f9ea64cd
notyourfather <applicatnccnt@gmail.com>
2022-08-07 16:55:14 +0800

    Resman will now look for versioned file.

--- renderer/resman.py
+++ renderer/resman.py
@@ -1,6 +1,10 @@
+from functools import cache
 import json
+from optparse import Option
 
-from importlib.resources import open_text, open_binary
+from renderer.utils import LOGGER
+from importlib.resources import open_text, open_binary, is_resource
+from struct import pack
 from PIL import Image
 from typing import Optional
 from PIL import ImageFont
@@ -9,25 +13,65 @@ from PIL import ImageFont
 class ResourceManager:
     """A resource manager."""
 
-    def __init__(self):
+    def __init__(self, version: str):
         """Initializes this class."""
         self._resources = {}
+        self._default_res = f"{__package__}.resources"
+        self._versions = version
 
-    def load_json(self, package: str, filename: str) -> dict:
+    def load_json(
+        self,
+        filename: str,
+        path: Optional[str] = None,
+    ) -> dict:
         """Loads a json file from the given package and converts its numeric
         keys to integer.
         """
-        with open_text(package, filename) as tr:
-            return json.load(tr, object_hook=self.key_converter)
+        key = f"{path}.{filename}"
+        if cached := self._resources.get(key, None):
+            return cached
 
-    def load_font(self, filename, package=f"{__package__}.resources", size=12):
-        with open_binary(package, filename) as fr:
-            return ImageFont.truetype(fr, size=size)
+        try:
+            res_package = f"{__package__}.versions.{self._versions}.resources"
+            res_package = res_package if not path else f"{res_package}.{path}"
+
+            if not is_resource(res_package, filename):
+                raise FileNotFoundError
+            LOGGER.debug(f"Versioned {filename} found.")
+        except FileNotFoundError:
+            res_package = self._default_res
+            res_package = res_package if not path else f"{res_package}.{path}"
+
+        with open_text(res_package, filename) as tr:
+            data = json.load(tr, object_hook=self.key_converter)
+            self._resources[key] = data
+            return data
+
+    def load_font(self, filename: str, path: Optional[str] = None, size=12):
+        key = f"{path}.{filename}.{size}"
+        if cached := self._resources.get(key, None):
+            return cached
+
+        try:
+            res_package = f"{__package__}.versions.{self._versions}.resources"
+            res_package = res_package if not path else f"{res_package}.{path}"
+
+            if not is_resource(res_package, filename):
+                raise FileNotFoundError
+            LOGGER.debug(f"Versioned {filename} found.")
+        except FileNotFoundError:
+            res_package = self._default_res
+            res_package = res_package if not path else f"{res_package}.{path}"
+
+        with open_binary(res_package, filename) as fr:
+            data = ImageFont.truetype(fr, size=size)
+            self._resources[key] = data
+            return data
 
     def load_image(
         self,
-        package: str,
         filename: str,
+        path: Optional[str] = None,
         nearest=False,
         size: Optional[tuple[int, int]] = None,
         rot: Optional[int] = None,
@@ -43,42 +87,48 @@ class ResourceManager:
         Returns:
             Image.Image: The loaded image.
         """
-        for_key = [package, filename]
+        key = [filename]
+
+        if path:
+            key = [path, filename]
 
         if size:
-            for_key.append(".".join(map(str, size)))
-            # key_name = (
-            #     f"{package}.{filename}.{'.'.join(map(str, size))}.{nearest}"
-            # )
-        # else:
-        #     for
-        #     key_name = f"{package}.{filename}.{nearest}"
-        if rot:
-            for_key.append(str(rot))
+            key.append("_".join(map(str, size)))
 
-        for_key.append(str(nearest))
-        key_name = ".".join(for_key)
+        if rot:
+            key.append(str(rot))
+
+        key.append(str(nearest))
+        key_name = "_".join(key)
 
         if key_name in self._resources:
             return self._resources[key_name].copy()
-        else:
-            with open_binary(package, filename) as br:
-                image = Image.open(br)
 
-                if image.mode != "RGBA":
-                    image = image.convert("RGBA")
+        try:
+            res_package = f"{__package__}.versions.{self._versions}.resources"
+            res_package = res_package if not path else f"{res_package}.{path}"
 
-                if size:
-                    image = image.resize(
-                        size, Image.LANCZOS if not nearest else Image.NEAREST
-                    )
-                if rot:
-                    image = image.rotate(
-                        rot, resample=Image.BICUBIC, expand=True
-                    )
+            if not is_resource(res_package, filename):
+                raise FileNotFoundError
+            LOGGER.debug(f"Versioned {filename} found.")
+        except (FileNotFoundError, ModuleNotFoundError):
+            res_package = self._default_res
+            res_package = res_package if not path else f"{res_package}.{path}"
 
-                self._resources[key_name] = image.copy()
-                return image.copy()
+        with open_binary(res_package, filename) as br:
+            image = Image.open(br)
+
+            if image.mode != "RGBA":
+                image = image.convert("RGBA")
+            if size:
+                image = image.resize(
+                    size, Image.LANCZOS if not nearest else Image.NEAREST
+                )
+            if rot:
+                image = image.rotate(rot, resample=Image.BICUBIC, expand=True)
+
+            self._resources[key_name] = image.copy()
+            return image.copy()
 
     @staticmethod
     def key_converter(o):
