9129dcf053199fca23d50df78f36827e532f4f27
notyourfather <applicatnccnt@gmail.com>
2022-08-07 16:55:39 +0800

    timeit

--- render.py
+++ render.py
@@ -1,7 +1,19 @@
 import pickle
+import timeit
 
 from renderer.render import Renderer
+
+setup = """
+from renderer.render import Renderer
+import pickle
+"""
+code = """
+with open("data.dat", "rb") as f:
+    Renderer(pickle.load(f)).start()
+"""
 
 if __name__ == "__main__":
     with open("data.dat", "rb") as f:
         Renderer(pickle.load(f)).start()
+    # result = timeit.timeit(code, setup, number=10)
+    # print(result)
