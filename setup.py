67c00214f9d7fa3dc440f61d392e647f08a1f15f
notyourfather <applicatnccnt@gmail.com>
2022-08-14 01:08:01 +0800

    Version bump.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="minimap-renderer",
-    version="0.0.1",
+    version="0.0.2",
     author="notyourfather, trackpad",
     description=(
         "Minimap Renderer parses World of Warships replays to create a "
