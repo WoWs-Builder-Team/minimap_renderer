a23078e779fe1cbc401b1ba9012cd5f6e23d1f86
notyourfather <applicatnccnt@gmail.com>
2022-08-31 20:37:05 +0800

    Parse and render is now combined.

--- render.py
+++ render.py
@@ -1,10 +1,23 @@
-import pickle
-
+from pathlib import Path
 from renderer.render import Renderer
+from replay_parser import ReplayParser
 
 
 if __name__ == "__main__":
-    with open("data.dat", "rb") as f:
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--replay", type=str, required=True)
+    namespace = parser.parse_args()
+    filename = Path(namespace.replay).stem
+    with open(namespace.replay, "rb") as f:
+        replay_info = ReplayParser(
+            f, strict=True, raw_data_output=False
+        ).get_info()
+
         Renderer(
-            pickle.load(f), logs=True, enable_chat=True, use_tqdm=True
-        ).start("minimap.mp4")
+            replay_info["hidden"]["replay_data"],
+            logs=True,
+            enable_chat=True,
+            use_tqdm=True,
+        ).start(f"{filename}.mp4")
