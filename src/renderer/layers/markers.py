4c1d30de113336a24039bba976d75b0c8fbcc834
notyourfather <applicatnccnt@gmail.com>
2022-09-01 20:45:15 +0800

    Added markers.

--- src/renderer/layers/markers.py
+++ src/renderer/layers/markers.py
@@ -1,33 +1,14 @@
 from typing import Optional
-from ..data import ReplayData
+from renderer.data import ReplayData
 from renderer.render import Renderer
 from renderer.base import LayerBase
-from renderer.const import RELATION_NORMAL_STR, COLORS_NORMAL
-from renderer.utils import (
-    generate_ship_data,
-    paste_args_centered,
-    draw_health_bar,
-)
-
+from renderer.const import RELATION_NORMAL_STR
 from PIL import Image, ImageDraw
-from math import hypot
 
-MIN_VIEW_DISTANCES = {
-    1: 15000,
-    2: 15000,
-    3: 17000,
-    4: 20000,
-    5: 23000,
-    6: 26000,
-    7: 27000,
-    8: 30000,
-    9: 33000,
-    10: 35000,
-    11: 35000,
-}
+NAME = {13: "radar", 11: "hydro"}
 
 
-class LayerShipBase(LayerBase):
+class LayerMarkersBase(LayerBase):
     """A class that handles/draws ships to the minimap.
 
     Args:
@@ -50,14 +31,7 @@ class LayerShipBase(LayerBase):
             replay_data if replay_data else self._renderer.replay_data
         )
         self._color = color
-        self._ship_info = generate_ship_data(
-            self._replay_data.player_info, renderer.resman, color
-        )
-        self._active_consumables: dict[int, dict[int, float]] = {}
         self._abilities = renderer.resman.load_json("abilities.json")
-        self._ships = renderer.resman.load_json("ships.json")
-        self._consumable_cache: dict[int, Image.Image] = {}
-        self._owner = self._replay_data.player_info[self._replay_data.owner_id]
 
     def draw(self, game_time: int, image: Image.Image):
         """Draws the ship icons to the minimap image.
@@ -66,5 +40,50 @@ class LayerShipBase(LayerBase):
             game_time (int): The game time.
             image (Image.Image): The minimap image.
         """
-        pass
+        events = self._replay_data.events
 
+        target_ids = set()
+
+        for i in self._renderer.conman.active_consumables.values():
+            target_ids.update(i)
+
+        if not target_ids.intersection({11, 13}):
+            return
+
+        for vehicle in sorted(
+            events[game_time].evt_vehicle.values(),
+        ):
+            if self._renderer.dual_mode and vehicle.relation == 1:
+                continue
+
+            player = self._replay_data.player_info[vehicle.player_id]
+            abilities = self._abilities[player.ship_params_id]
+            aid_to_subtype = abilities["id_to_subtype"]
+            x, y = self._renderer.get_scaled((vehicle.x, vehicle.y))
+
+            if ac := self._renderer.conman.active_consumables.get(
+                vehicle.vehicle_id, None
+            ):
+                if not vehicle.is_visible or not vehicle.is_alive:
+                    continue
+
+                if self._color:
+                    relation = 0 if self._color == "green" else 1
+                else:
+                    relation = player.relation
+
+                relation_str = RELATION_NORMAL_STR[relation]
+
+                for aid in {11, 13}.intersection(ac):
+                    dist_ship_bw = abilities[aid_to_subtype[aid]]["distShip"]
+                    r = round(self._renderer.get_scaled_r(dist_ship_bw) / 2)
+                    w = h = r * 2
+
+                    filename = f"{NAME[aid]}_{relation_str}.png"
+
+                    marker = self._renderer.resman.load_image(
+                        filename, "markers", size=(w, h)
+                    )
+                    image.alpha_composite(
+                        marker, (x - marker.width // 2, y - marker.height // 2)
+                    )
