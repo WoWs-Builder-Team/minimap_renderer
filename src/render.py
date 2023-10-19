9d6a580bbb39ae95991975ba74f4dce08c7751c1
ReinWD <ReinWDD@gmail.com>
2023-10-20 02:20:04 +0800

    main method

--- src/render.py
+++ src/render.py
@@ -33,30 +33,10 @@ def run(pathStr, fps: int = 60, speed_scale: float = 20.0):
         LOGGER.info("Done.")
 
 if __name__ == "__main__":
-    # run("/home/reinwd/replays/20231014/20231014_191413_PZSD110-Hsiang-Yang_54_Faroe.wowsreplay")
-
-    # run("/home/reinwd/replays//20231016/king-of-the-sea-xvi_apac_groups-stage_group-4_round-1_xmm-vs-prpru_game-2_xmm.wowsreplay")
-    # run("/home/reinwd/replays//20231016/king-of-the-sea-xvi_apac_groups-stage_group-4_round-3_xmm-vs-kusa_game-3_xmm.wowsreplay")
-    # run("/home/reinwd/replays//20231016/king-of-the-sea-xvi_apac_groups-stage_group-4_round-3_xmm-vs-kusa_game-1_xmm.wowsreplay")
-    # run("/home/reinwd/replays//20231016/king-of-the-sea-xvi_apac_groups-stage_group-4_round-2_xmm-vs-nyako_game-3_xmm.wowsreplay")
-    # run("/home/reinwd/replays//20231016/king-of-the-sea-xvi_apac_groups-stage_group-4_round-1_xmm-vs-prpru_game-1_xmm.wowsreplay")
-    # run("/home/reinwd/replays//20231016/king-of-the-sea-xvi_apac_groups-stage_group-4_round-3_xmm-vs-kusa_game-2_xmm.wowsreplay")
-    # run("/home/reinwd/replays//20231016/king-of-the-sea-xvi_apac_groups-stage_group-4_round-2_xmm-vs-nyako_game-1_xmm.wowsreplay")
-    run("/home/reinwd/replays//20231020/20231020_010901_PFSC111-Conde_22_tierra_del_fuego.wowsreplay")
-    
-    
-    
-    
-    
-    
-    
-    
-    
-  # import argparse
-
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("--replay", type=str, required=True)
-    # parser.add_argument("--fps", type=int, required=False, default=60)
-    # parser.add_argument("--speed", type=float, required=False, default=20.0)
-    # namespace = parser.parse_args()
-    # run(namespace.replay, namespace.fps, namespace.speed)
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--replay", type=str, required=True)
+    parser.add_argument("--fps", type=int, required=False, default=60)
+    parser.add_argument("--speed", type=float, required=False, default=20.0)
+    namespace = parser.parse_args()
+    run(namespace.replay, namespace.fps, namespace.speed)
