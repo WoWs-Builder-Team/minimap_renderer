8f011bbe655d6195effca2b7f0ffc230fb1a29dc
JustOneSummer <myyuyukosama@gmail.com>
2024-04-27 11:06:30 +0800

    新增http basic验证 temp文件夹移动到根目录

--- src/render_web.py
+++ src/render_web.py
@@ -1,19 +1,49 @@
 import io
+import json
 import os
+import secrets
 import uuid
+from typing import Annotated
 
-from fastapi import FastAPI, UploadFile, BackgroundTasks
+from fastapi import FastAPI, UploadFile, BackgroundTasks, Depends, HTTPException, status
 from fastapi.responses import FileResponse
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 from renderer.render import Renderer
 from replay_parser import ReplayParser
 
 app = FastAPI()
+security = HTTPBasic()
+
+
+def get_current_username(
+        credentials: Annotated[HTTPBasicCredentials, Depends(security)],
+):
+    with open(os.path.abspath(os.path.dirname(os.path.dirname(__file__))) + "/token.json", 'r', encoding='utf8') as json_f:
+        json_data = json.load(json_f)
+    print(json_data)
+    current_username_bytes = credentials.username.encode("utf8")
+    correct_username_bytes = str(json_data['username']).encode("utf8")
+    is_correct_username = secrets.compare_digest(
+        current_username_bytes, correct_username_bytes
+    )
+    current_password_bytes = credentials.password.encode("utf8")
+    correct_password_bytes = str(json_data['password']).encode("utf8")
+    is_correct_password = secrets.compare_digest(
+        current_password_bytes, correct_password_bytes
+    )
+    if not (is_correct_username and is_correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
 
 
 @app.post("/uploadRep")
-async def upload_rep(file: UploadFile):
-    uuid_str_mp4 = os.path.dirname(os.path.abspath(__file__)) + '/temp/' + str(uuid.uuid1()) + '.mp4'
+async def upload_rep(file: UploadFile, username: Annotated[str, Depends(get_current_username)]):
+    uuid_str_mp4 = os.path.abspath(os.path.dirname(os.path.dirname(__file__))) + '/temp/' + str(uuid.uuid1()) + '.mp4'
     binary_stream = io.BytesIO(file.file.read())
     replay_info = ReplayParser(
         binary_stream, strict=True, raw_data_output=False
